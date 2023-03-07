c441c2dd740e43110cae1229b7b0607999766df3
Anish Kumar <anishbishnoi127@gmail.com>
2023-03-07 20:40:40 +0530

    react marathon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+import PlayButton from "./components/PlayButton";
+import Video from "./components/Video";
+import videoDB from "./data/data";
 function App() {
+  console.log("render App");
+
+  const [videos, setVideos] = useState(videoDB);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" onClick={() => console.log("App")}>
+      <div>
+        <button
+          onClick={() => {
+            setVideos([
+              ...videos,
+              {
+                id: videos.length + 1,
+                title: "Demo JS tutorial",
+                views: "1M",
+                time: "1 month ago",
+                channel: "Coder Dost",
+                verified: true,
+              },
+            ]);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add Video
+        </button>
+      </div>
+      {videos.map((video) => (
+        <Video
+          key={video.id}
+          title={video.title}
+          views={video.views}
+          time={video.time}
+          channel={video.channel}
+          verified={video.verified}
+          id={video.id}
+        >
+          <PlayButton
+            onPlay={() => console.log("Playing..", video.title)}
+            onPause={() => console.log("Paused..", video.title)}
+          >
+            {video.title}
+          </PlayButton>
+        </Video>
+      ))}
+
+      <div style={{ clear: "both" }}>
+        {/* <PlayButton message="play-msg" onPlay={()=>console.log('Play')} onPause={()=>console.log('Pause')}>Play</PlayButton> */}
+
+        {/* <PlayButton message="pause-msg" onSmash={()=>alert('Playyy')}>Pause</PlayButton> */}
+      </div>
+
+      <Counter></Counter>
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+const videos = [
+  {
+    id: 1,
+    title: "React JS tutorial",
+    views: "999K",
+    time: "1 year ago",
+    channel: "Anish Bishnoi",
+    verified: true,
+  },
+  {
+    id: 2,
+    title: "Node JS tutorial",
+    views: "100K",
+    time: "1 year ago",
+    channel: "Anish Bishnoi",
+    verified: false,
+  },
+  {
+    id: 3,
+    title: "MongoDb JS tutorial",
+    views: "1M",
+    time: "1 month ago",
+    channel: "Anish Bishnoi",
+    verified: true,
+  },
+];
+
+export default videos;
